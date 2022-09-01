7a195e4921a17891fd167cc4c5a3353ae50d5577
Liam Jogal <liamjogal@Liams-MacBook-Pro.local>
2022-09-01 03:24:16 -0500

    datepicker

--- frontend/src/components/exercise_data/pages/Exercise.js
+++ frontend/src/components/exercise_data/pages/Exercise.js
@@ -1,6 +1,61 @@
 import * as React from "react";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
+import dayjs from 'dayjs';
 import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import {Select} from "@mui/material";
+import {InputLabel} from "@mui/material";
+import {Box} from "@mui/material";
+import {FormControl, MenuItem} from "@mui/material";
+
+
+
+function DatePicker() {
+  const [value, setValue] = React.useState(dayjs('2014-08-18T21:11:54'));
+
+  const handleChange = (newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DateTimePicker
+          label="Date&Time picker"
+          value={value}
+          onChange={handleChange}
+          renderInput={(params) => <TextField {...params} />}
+        />
+    </LocalizationProvider>
+  );
+}
+
+ const ExcerciseSelector = () => {
+    const [excercise, setExcercise] = React.useState('');
+  
+    const handleChange = (event) => {
+      setExcercise(event.target.value);
+    };
+  
+    return (
+      <>
+          <InputLabel id="select-label">Excercise</InputLabel>
+          <Select
+            labelId="select-label"
+            id="excercise-selector"
+            value={excercise}
+            label="Excercise"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </>
+ 
+    );
+  }
 
 
 
@@ -13,15 +68,33 @@ import TextField from '@mui/material/TextField';
 export default function Exercise() {
 
     return (
-        <Grid>
+        <>
+        {/* <h1>New Exercise</h1>
+        <Box justifyContent="center">
+            <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+}}> */}
             <h1>New Exercise</h1>
-            <TextField id="date-input" label="Date" variant="outlined" />  {/* TODO: Change later to calendar component */}
-            <TextField id="exercise-input" label="Exercise" variant="outlined" />
+            <Container>
+                {/* <Box
+                alignItems="center"
+                justifyContent="center"> */}
+         
+            <DatePicker id="date-input"></DatePicker>
+            <ExcerciseSelector id="excercise-input"></ExcerciseSelector>
             <TextField id="weight-input" label="Weight" variant="outlined" />
             <TextField id="reps-input" label="Reps" variant="outlined" />
             <TextField id="sets-input" label="Sets" variant="outlined" />
-        </Grid>
-        
+
+            {/* </Box> */}
+            
+            </Container>
+            {/* </div>
+            </Box> */}
+            
+        </>        
 
 
             
